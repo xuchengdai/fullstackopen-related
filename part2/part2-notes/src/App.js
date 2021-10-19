74114b7421657bf95587e4daad9d548c7f15090d
Chris Dai <chrisdai@Chriss-MacBook-Air.local>
2021-10-19 21:20:33 +0800

    before filter

---
+++ part2/part2-notes/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+    console.log(newNote);
+  };
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
